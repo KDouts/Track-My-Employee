2b0a969a5accbe12219adf3e129c693a65318eed
Kurtis Doute <kcd257@yahoo.com>
2022-02-05 13:46:37 -0800

    added what will happen when each choice is picked from the list of options

--- server.js
+++ server.js
@@ -23,6 +23,91 @@ const promptUser = () => {
                   'No Action']
       }
     ])
-}
+    .then((answers) => {
+        const { choices } = answers; 
+  
+        if (choices === "View all departments") {
+          showDepartments();
+        }
+  
+        if (choices === "View all roles") {
+          showRoles();
+        }
+  
+        if (choices === "View all employees") {
+          showEmployees();
+        }
+  
+        if (choices === "Add a department") {
+          addDepartment();
+        }
+  
+        if (choices === "Add a role") {
+          addRole();
+        }
+  
+        if (choices === "Add an employee") {
+          addEmployee();
+        }
+  
+        if (choices === "Update an employee role") {
+          updateEmployee();
+        }
+  
+        if (choices === "Update an employee manager") {
+          updateManager();
+        }
+  
+        if (choices === "View employees by department") {
+          employeeDepartment();
+        }
+  
+        if (choices === "Delete a department") {
+          deleteDepartment();
+        }
+  
+        if (choices === "Delete a role") {
+          deleteRole();
+        }
+  
+        if (choices === "Delete an employee") {
+          deleteEmployee();
+        }
+  
+        if (choices === "View department budgets") {
+          viewBudget();
+        }
+  
+        if (choices === "No Action") {
+          connection.end()
+      };
+    });
+  };
+  
+  showDepartments = () => {
+    console.log('Showing all departments...\n');
+    const sql = `SELECT department.id AS id, department.name AS department FROM department`; 
+  
+    connection.promise().query(sql, (err, rows) => {
+      if (err) throw err;
+      console.table(rows);
+      promptUser();
+    });
+  };
+  
+  showRoles = () => {
+    console.log('Showing all roles...\n');
+  
+    const sql = `SELECT role.id, role.title, department.name AS department
+                 FROM role
+                 INNER JOIN department ON role.department_id = department.id`;
+    
+    connection.promise().query(sql, (err, rows) => {
+      if (err) throw err; 
+      console.table(rows); 
+      promptUser();
+    })
+  };
+
 
 promptUser();
