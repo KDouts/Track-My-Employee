2c76574ce81e55aaf3d2e61a918b937e543458a3
Kurtis Doute <kcd257@yahoo.com>
2022-02-05 13:35:56 -0800

    added directory to choose from in the command line

---
+++ server.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const promptUser = () => {
+    inquirer.prompt ([
+      {
+        type: 'list',
+        name: 'choices', 
+        message: 'What would you like to do?',
+        choices: ['View all departments', 
+                  'View all roles', 
+                  'View all employees', 
+                  'Add a department', 
+                  'Add a role', 
+                  'Add an employee', 
+                  'Update an employee role',
+                  'Update an employee manager',
+                  "View employees by department",
+                  'Delete a department',
+                  'Delete a role',
+                  'Delete an employee',
+                  'View department budgets',
+                  'No Action']
+      }
+    ])
+}
+
+promptUser();
